db6cab3d74a9e397f881245d9b6336235ccb3371
Thyago Barbosa-Rodrigues <thyagobr@gmail.com>
2019-10-23 07:22:25 +0200

    Saves file

--- src/map.rb
+++ src/map.rb
@@ -1,4 +1,5 @@
 require 'gosu'
+require 'byebug'
 
 class Map < Gosu::Window
   WIDTH = 800
@@ -7,13 +8,23 @@ class Map < Gosu::Window
   MAP_HEIGHT = HEIGHT * 2
   TILE_SIZE = 16
 
-  def initialize
+  TILES = [
+    0xbb_00aa00,
+    0xbb_f0aa00
+  ]
+
+  def initialize(**args)
     super(WIDTH, HEIGHT, fullscreen = false)
-    @map = []
-    (HEIGHT / TILE_SIZE).times do |y|
-      @map << []
+    if args[:map_file]
+      puts "Loading file: #{args[:map_file]}"
+      @map = load_file(args[:map_file])
+    else
+      @map = []
+
       (WIDTH / TILE_SIZE).times do |x|
-        @map[y] << Gosu::Color.argb(0xbb_00aa00)
+        (HEIGHT / TILE_SIZE).times do |y|
+          @map[x + y * (WIDTH / TILE_SIZE)] = 0
+        end
       end
     end
   end
@@ -25,21 +36,35 @@ class Map < Gosu::Window
     save_map if id == Gosu::KbS
   end
 
+  def save_map
+    File.write("save_file", @map.join('|'))
+    puts "Saved."
+  end
+
+  def load_file(map_file_name)
+    file_content = File.read(map_file_name)
+    file_content.split("|").map(&:to_i)
+  rescue Errno::ENOENT
+    puts "File not found."
+    []
+  end
+
   def left_click
-    x = mouse_x / TILE_SIZE
-    y = mouse_y / TILE_SIZE
-    @map[y][x] = Gosu::Color.argb(0xbb_f0aa00)
+    x = mouse_x.to_i / TILE_SIZE
+    y = mouse_y.to_i / TILE_SIZE
+    @map[x + y * (WIDTH / TILE_SIZE)] = 1
+    puts "#{x},#{y}"
   end
 
   def draw
     left_click if button_down?(Gosu::MsLeft)
 
-    (WIDTH / TILE_SIZE).times do |x|
+    (WIDTH  / TILE_SIZE).times do |x|
       (HEIGHT / TILE_SIZE).times do |y|
-        Gosu.draw_rect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE, @map[y][x])
+        Gosu.draw_rect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE, Gosu::Color.argb(TILES[@map[x + y * (WIDTH / TILE_SIZE)]]))
       end
     end
   end
 end
 
-Map.new.show
+Map.new(map_file: "save_file").show
