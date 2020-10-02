c2d2d758bb3eb7c2958373cfe420e5b4f0937fd6
Thyago Barbosa-Rodrigues <thyago.barbosa@daliaresearch.com>
2020-10-02 12:51:58 +0200

    Saves JSON

--- game.js
+++ game.js
@@ -47,23 +47,59 @@
     }
   }
 
-  // Load
-  ctx.strokeStyle = 'black';
-  ctx.strokeRect(5, canvas.height - 160, 100, 40);
-  ctx.font = '24px serif';
-  ctx.fillText('Load', 15, canvas.height - 130);
-  // Save
-  ctx.strokeStyle = 'black';
-  ctx.strokeRect(5, canvas.height - 110, 100, 40);
-  ctx.font = '24px serif';
-  ctx.fillText('Save', 15, canvas.height - 80);
-  // Export
-  ctx.strokeStyle = 'black';
-  ctx.strokeRect(5, canvas.height - 60, 100, 40);
-  ctx.font = '24px serif';
-  ctx.fillText('Export', 15, canvas.height - 30);
+  buttons = [
+    {
+      label: "Load",
+      rect: { x: 5, y: canvas.height - 160, width: 100, height: 40 }
+    },
+    {
+      label: "Save",
+      rect: { x: 5, y: canvas.height - 110, width: 100, height: 40 },
+      perform: function() {
+        var data = map_tiles.slice();
+        filename = "map.json";
+
+        if (!data) {
+          console.error('No data')
+          return;
+        }
+
+        if (!filename) filename = 'console.json'
+
+        if (typeof data === "object") {
+          data = JSON.stringify(data, undefined, 4)
+        }
+
+        var blob = new Blob([data], { type: 'text/json' }),
+          e = document.createEvent('MouseEvents'),
+          a = document.createElement('a')
+
+        a.download = filename
+        a.href = window.URL.createObjectURL(blob)
+        a.dataset.downloadurl = ['text/json', a.download, a.href].join(':')
+        e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null)
+        a.dispatchEvent(e)
+      }
+    },
+    {
+      label: "Open",
+      rect: { x: 5, y: canvas.height - 60, width: 100, height: 40 }
+    }
+  ]
+
+  buttons.forEach(function (button) {
+    ctx.strokeStyle = 'black';
+    ctx.strokeRect(button.rect.x, button.rect.y, button.rect.width, button.rect.height);
+    ctx.font = '24px serif';
+    var text_width = ctx.measureText(button.label).width;
+    var font_size = 24;
+    var placement_x = (button.rect.x + (button.rect.width / 2) - (text_width / 2));
+    var placement_y = (button.rect.y + (button.rect.height / 2) + (font_size/3));
+    ctx.fillText(button.label, placement_x, placement_y);
+  });
 
   ctx.beginPath();
+
   for (var y = 0; y < tiles_y; y++) {
     for (var x = 0; x < tiles_x; x++) {
       map_tiles[(y * tiles_x) + x] = {
@@ -101,7 +137,19 @@
     let tile = tiles.find(function (tile) {
       return is_within_canvas_bounds(event, tile);
     })
-    if (tile) current_tile = tile.tile;
+    if (tile) {
+      current_tile = tile.tile;
+      // set somewhere which color is chosen
+      return;
+    }
+    const button_clicked = buttons.find(function(button) {
+      return is_within_canvas_bounds(event, button.rect);
+    });
+    if (button_clicked)
+    {
+      console.log(button_clicked.label)
+      button_clicked.perform();
+    }
     // Drawing board side
     else {
       const drawing_board_dimensions = { x: 110, width: canvas.width - 110, y: 50, height: canvas.height - 50 }
